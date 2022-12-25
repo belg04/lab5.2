48e1f05236810d919137190783e9ee745393ebf6
glebe <glebe@BELG04>
2022-12-26 01:57:42 +0200

    Добавьте файлы проекта.

---
+++ lab5.2/Program.cs
@@ -0,0 +1,6 @@
+﻿char[] input = Console.ReadLine().ToCharArray();
+char t = input[0];
+input[0] = input[input.Length - 1];
+input[input.Length - 1] = t;
+Console.WriteLine(new string(input));
+Console.ReadKey();
